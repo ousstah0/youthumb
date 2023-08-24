c3c3368cd1d0751d69a72c78efa653e6bb7cb81f
ousstah0 <142893077+ousstah0@users.noreply.github.com>
2023-08-24 17:30:21 +0100

    add

--- pages/_document.js
+++ pages/_document.js
@@ -23,16 +23,41 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          
-          {/* Add the meta tag for galaksion-domain-verification */}
-          <meta name="galaksion-domain-verification" content="04357e8af7bd3f94812cf4b3cddd0286562431c55aa9ec0e5f747e305d6d9d33" />
         </Head>
-        <body>
-          <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "1rem", backgroundColor: "#fcfcfc", color: "#0a0a0a" }}>
-            <h1 style={{ fontSize: "1.5rem" }}>Download Youtube Thumbnail</h1>
-            <a href="https://sites.google.com/view/thumb-download/home" style={{ color: "#0a0a0a", textDecoration: "none" }}>Privacy Policy</a>
+        <body style={{ margin: 0, padding: 0, fontFamily: "Arial, sans-serif" }}>
+          <header
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: "1rem",
+              backgroundColor: "#03a9f4",
+              color: "#fff",
+            }}
+          >
+            <h1 style={{ fontSize: "1.5rem" }}>Download YouTube Thumbnail</h1>
+            <a
+              href="https://sites.google.com/view/thumb-download/home"
+              style={{
+                color: "#fff",
+                textDecoration: "none",
+                fontSize: "0.9rem",
+              }}
+            >
+              Privacy Policy
+            </a>
           </header>
           <Main />
+          <footer
+            style={{
+              padding: "1rem",
+              backgroundColor: "#03a9f4",
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            &copy; {new Date().getFullYear()} Your Website Name. All rights reserved.
+          </footer>
           <NextScript />
         </body>
       </Html>
